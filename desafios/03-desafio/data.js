5c01e85679e05e4bf3a1de1ad1ab1e55815ac32a
F1984898 Celso Rodrigues de Freitas Junior <celsojunior@bb.com.br>
2021-09-02 16:16:21 -0300

    Utilizando dados do server e entrega desafio 3-2

---
+++ desafios/03-desafio/data.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    id: "substituindo-a-instrucao-switch-por-object-literal",
+    image: "https://blog.rocketseat.com.br/content/images/2020/09/substituindo-a-instrucao-switch-por-object-literal-rocketseat-1.png",
+    title: "Substituindo a instrução switch por Object Literal",
+    role: "Author: Thiago Marinho",
+    duration: "2 min"
+  },
+  {
+    id: "es11-novas-features-do-javascript",
+    image: "https://blog.rocketseat.com.br/content/images/2020/09/es11-novas-features-do-javascript.png",
+    title: "ES11 - Novas features do JavaScript",
+    role: "Author: Thiago Marinho",
+    duration: "8 min"
+  },
+  {
+    id: "o-que-e-readme-e-porque-e-tao-importante",
+    image: "https://blog.rocketseat.com.br/content/images/2020/07/o-que-e-readme.png",
+    title: "O que é README e porque é tão importante",
+    role: "Author: Thiago Marinho",
+    duration: "4 min"
+  },
+]
--- modulo03/server.js
+++ modulo03/server.js
@@ -2,21 +2,35 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require('./data');
 
-server.use(express.static('public'))
+server.use(express.static('public'));
 
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-  express: server
-})
+  express: server,
+  autoescape: false
+});
 
 server.get("/", function(request, response) {
-  return response.render("about");
+  const about = {
+    avatar_url : "https://avatars3.githubusercontent.com/u/6643122?s=460&v=4",
+    name : "Mayk Brito",
+    role : "Instrutor - Rocketseat",
+    description: 'Programador full-stack, focado em trazer o melhor ensino para iniciantes em programação. Colaborador da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+    links: [
+      { name: "Github", url: "https://github.com/maykbrito/" },
+      { name: "Twitter", url: "https://twitter.com/maykbrito/" },
+      { name: "Linkedin", url: "https://www.linkedin.com/in/maykbrito/" },
+    ]
+
+  }
+  return response.render("about", { about });
 });
 
 server.get("/portfolio", function (request, response) {
-  return response.render("portfolio");
+  return response.render("portfolio", { items: videos });
 })
 
 server.listen(5000, function() {
